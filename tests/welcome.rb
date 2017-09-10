03e74254da104efcf9e998d9e1d4267435ca62c6
StellarDoor5319 <danthwaites30@btinternet.com>
2017-09-10 10:46:56 +0100

    Added first test
    Added a test that checks main.rb's GetWelcome function returns
    the expected result.

---
+++ tests/welcome.rb
@@ -0,0 +1,10 @@
+require_relative "./main.rb"
+require "test/unit"
+
+class TestWelcome < Test::Unit::TestCase
+
+	def test_print
+		assert_equal("Welcome to Docilater, John!", GetWelcome("John"))
+	end
+
+end
