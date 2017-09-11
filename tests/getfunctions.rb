089bac3f73f9d5f11da09b3992b68907e6282a75
StellarDoor5319 <danthwaites30@btinternet.com>
2017-09-11 18:20:06 +0100

    Added getFunctions test
    Added a simple test to check if the getFunctions function is working as
    expected.

---
+++ tests/getfunctions.rb
@@ -0,0 +1,9 @@
+require_relative "../main.rb"
+require "test/unit"
+
+class testGetFunctions << Test::Unit::TestCase
+
+	def test_functionsGot
+		assert_equal(["function1", "function2(input1)"], getFunctions("def function1\n        #do something\nend\n\ndef function2(input1)\n        #do something else\nend"), "Function selection doesn't work")
+	end
+end
