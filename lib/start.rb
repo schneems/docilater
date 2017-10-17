bf09eb9790f8763265fbf025963350b42168e13e
StellarDoor5319 <danthwaites30@btinternet.com>
2017-10-17 17:25:24 +0100

    Added comments
    Added some comments to start.rb, for better documentation.

--- lib/start.rb
+++ lib/start.rb
@@ -1,3 +1,6 @@
 require_relative "main.rb"
 
+# Run this file when you want to start the program. It will automatically launch the code from other files to run the program correctly.
+# @note Don't run main.rb accidentally!
+
 start()
